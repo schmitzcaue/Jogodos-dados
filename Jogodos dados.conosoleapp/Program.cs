c520e415df368e50bdfe185cf8e474b63ff0d8df
caues <caues@SCHMITZNOT>
2025-04-01 19:14:58 -0300

    Add project files.

---
+++ Jogodos dados.conosoleapp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Jogodos_dados.conosoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
