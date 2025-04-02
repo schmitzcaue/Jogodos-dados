578be31d8ad1d5c67f7644cb319cbc5e503ab091
caues <caues@SCHMITZNOT>
2025-04-01 21:30:56 -0300

    ○ Avanço extra:

--- Jogodos dados.conosoleapp/Program.cs
+++ Jogodos dados.conosoleapp/Program.cs
@@ -1,15 +1,10 @@
 ﻿namespace Jogodos_dados.ConosoleApp
 {
-    /*  Versão 3 - Incluir o computador como oponente 
-    Requisitos:
-
-    Informar que o computador está jogando
-    Armazenar a posição do computador na pista e atualizar o valor após o lançamento do dado
-    Atualizar a posição do computador após seu lançamento de dado
-    Exibir a nova posição
-    Verificar se o computador alcançou ou ultrapassou a linha de chegada
-    Informar quem venceu o jogo
-    Implementar turnos alternados entre jogador e computador
+    /*  Versão 4. Para tornar o jogo mais interessante, algumas posições na pista podem ter eventos especiais:
+    ○ Avanço extra: 
+    Se o competidor parar em uma posição específica (ex.: 5, 10, 15), ele avança +3 casas.
+    ○ Recuo: Se o competidor parar em outra posição específica (ex.: 7, 13, 20), ele recua -2 casas.
+    ○ Rodada extra: Se o competidor tirar 6 no dado, ele ganha uma rodada extra.
      */
 
     internal class Program
@@ -31,13 +26,32 @@
                     // Turno do usuario
                     ExibirCabecalho("Usuario");
 
-                    int resultadoDoComputador = LancarDado();
+                    int resultadoDoUsuario = LancarDado();
 
-                    ExibirResultadoDoSorteio(resultadoDoComputador);
+                    ExibirResultadoDoSorteio(resultadoDoUsuario);
 
-                    posicaoUsuario += resultadoDoComputador;
+                    posicaoUsuario += resultadoDoUsuario;
 
                     Console.WriteLine($"O jogador esta na posicao: {posicaoUsuario} de {limiteLinhaChegada}");
+                    if (posicaoUsuario == 5 || posicaoUsuario == 10 || posicaoUsuario == 15)
+                    {
+                        Console.WriteLine("O usuario avaçou 3 casas");
+                        posicaoUsuario += 3;
+                    }
+                    if (posicaoUsuario == 7 || posicaoUsuario == 13 || posicaoUsuario == 20 )
+                    {
+                        Console.WriteLine("O usuario recuou 2 casas");
+                        posicaoUsuario -= 2;
+                    }
+                    if (resultadoDoUsuario == 6)
+                    {
+                        Console.WriteLine("O jogador ganhou uma rodada extra");
+                        Console.WriteLine("--------------------");
+                        Console.Write("Pressione  ENTER para continuar");
+                        Console.ReadLine();
+                        continue;
+                    }
+
 
                     if (posicaoUsuario >= limiteLinhaChegada)
                     { 
@@ -47,9 +61,8 @@
                         jogoEstaEmAndamento = false;
                         continue;
                     }
-                    else
+                   
 
-                        Console.WriteLine($"O jogador esta na posicao: {posicaoUsuario} de {limiteLinhaChegada}");
                     Console.Write("Pressione  ENTER para continuar");
                     Console.ReadLine();
 
@@ -62,8 +75,31 @@
                     ExibirResultadoDoSorteio(resultadoComputador);
 
                     posicaoComputador += resultadoComputador;
+                    
 
-                    Console.WriteLine($"O jogador esta na posicao: {posicaoUsuario} de {limiteLinhaChegada}");
+                    Console.WriteLine($"O computador esta na posicao: {posicaoComputador} de {limiteLinhaChegada}");
+
+                    if (posicaoComputador == 5 || posicaoComputador == 10 || posicaoComputador == 15)
+                    {
+                        Console.WriteLine("O computador avaçou 3 casas");
+                        posicaoComputador += 3;
+                    }
+                    if (posicaoComputador == 7 || posicaoComputador == 13 || posicaoComputador == 20)
+                    {
+                        Console.WriteLine("O computador recuou 2 casas");
+                        posicaoComputador -= 2;
+                    
+                    }
+                    if (resultadoComputador == 6)
+                    {
+                        Console.WriteLine("O computador ganhou uma rodada extra");
+                        Console.WriteLine("--------------------");
+                        Console.Write("Pressione  ENTER para continuar");
+                        Console.ReadLine();
+                        continue;
+                    }
+
+
 
                     if (posicaoComputador >= limiteLinhaChegada)
                     {
@@ -74,9 +110,7 @@
                         continue;
 
                     }
-                    else
-
-                        Console.WriteLine($"O computador esta na posicao: {posicaoComputador} de {limiteLinhaChegada}");
+                    
                     Console.Write("Pressione  ENTER para continuar");
                     Console.ReadLine();
                 }
@@ -104,8 +138,6 @@
                 Console.ReadLine();
             }
 
-            Console.Write("Pressione ENTER para lançar o dado...");
-            Console.ReadLine();
         }
 
         static int LancarDado()
@@ -120,7 +152,6 @@
         static void ExibirResultadoDoSorteio(int resultado)
         {
             //exibe o resultado
-            Console.Clear();
             Console.WriteLine("--------------------");
             Console.WriteLine($" O valor sorteado foi: {resultado}");
             Console.WriteLine("--------------------");
